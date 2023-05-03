4644d06a641ff8df0d62de4e41877d2539b7de00
Angel Daniel (Kaniltazo) <angeldanielrm@icloud.com>
2023-05-02 21:51:15 -0500

    agregar API

--- src/acquisitions.js
+++ src/acquisitions.js
@@ -1,21 +1,25 @@
 import Chart from 'chart.js/auto';
 import {incrementarValor} from './funciones.js';
+import { getAquisitionsByYear } from './api'
 
 (async function () {
-    const data = [
-        { year: 2010, count: 10 },
-        { year: 2011, count: 20 },
-        { year: 2012, count: 15 },
-        { year: 2013, count: 25 },
-        { year: 2014, count: 22 },
-        { year: 2015, count: 30 },
-        { year: 2016, count: 28 },
-    ].map(fila => ( { ...fila, count: incrementarValor(fila.count) } ) );
+    // const data = [
+    //     { year: 2010, count: 10 },
+    //     { year: 2011, count: 20 },
+    //     { year: 2012, count: 15 },
+    //     { year: 2013, count: 25 },
+    //     { year: 2014, count: 22 },
+    //     { year: 2015, count: 30 },
+    //     { year: 2016, count: 28 },
+    // ].map(fila => ( { ...fila, count: incrementarValor(fila.count) } ) );
+
+    const data = await getAquisitionsByYear();
 
     new Chart(
         document.getElementById('acquisitions'),
         {
-            type: 'bar',
+            //bar, line, doughnut, radar, polararea, bubble
+            type: 'bubble',
             data: {
                 labels: data.map(row => row.year),
                 datasets: [
