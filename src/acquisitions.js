58da4137f13af2e00b73171d7acad751cd036459
Angel Daniel (Kaniltazo) <angeldanielrm@icloud.com>
2023-05-04 17:53:37 -0500

    Ajustes finales

--- src/acquisitions.js
+++ src/acquisitions.js
@@ -3,16 +3,7 @@ import { incrementarValor } from './funciones.js';
 import { getAquisitionsByYear } from './api'
 
 (async function () {
-    // const data = [
-    //     { year: 2010, count: 10 },
-    //     { year: 2011, count: 20 },
-    //     { year: 2012, count: 15 },
-    //     { year: 2013, count: 25 },
-    //     { year: 2014, count: 22 },
-    //     { year: 2015, count: 30 },
-    //     { year: 2016, count: 28 },
-    // ].map(fila => ( { ...fila, count: incrementarValor(fila.count) } ) );
-
+    
     const data = await fetch('https://sample-api-29g5.onrender.com/libros')
     .then(res => res.json())
     .then(data => {
@@ -21,8 +12,7 @@ import { getAquisitionsByYear } from './api'
             new Chart(
                 document.getElementById('acquisitions'),
                 {
-                    //bar, line, doughnut, radar, polararea, bubble
-                    type: 'bar',
+                type: 'bar',
                     data: {
                         labels: registros.map(row => row.title),
                         datasets: [
@@ -37,4 +27,4 @@ import { getAquisitionsByYear } from './api'
         })();
 
 
-})
+    })();
